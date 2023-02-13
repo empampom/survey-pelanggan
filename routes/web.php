1b4003819f4a2ebafc0d11ea33c7fa8cae1e024c
Syam Apriansyah <syamapriansyah@gmail.com>
2023-02-13 18:08:40 +0700

    ubah lagi route nya

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SurveyEksekutifController;
+// use App\Http\Controllers\SurveyEksekutifController;
 
-Route::controller(SurveyEksekutifController::class)->group(function () {
-    Route::get('/{id?}', 'form')->name('tulip.form');
-    Route::post('/{id?}', 'action')->name('tulip.action');
-});
+Route::view('/', 'tulip/index');
+
+// Route::controller(SurveyEksekutifController::class)->group(function () {
+//     Route::get('/', 'form')->name('tulip.form');
+//     Route::post('/', 'action')->name('tulip.action');
+// });
