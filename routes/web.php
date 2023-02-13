583be859aa7f015e84ef96e6d2f3e1a4cf49768e
Syam Apriansyah <syamapriansyah@gmail.com>
2023-02-13 17:34:20 +0700

    ubah route ke root

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SurveyEksekutifController;
 
-Route::prefix('tulip')->group(function () {
-    Route::controller(SurveyEksekutifController::class)->group(function () {
-        Route::get('/{id?}', 'form')->name('tulip.form');
-        Route::post('/{id?}', 'action')->name('tulip.action');
-    });
+Route::controller(SurveyEksekutifController::class)->group(function () {
+    Route::get('/{id?}', 'form')->name('tulip.form');
+    Route::post('/{id?}', 'action')->name('tulip.action');
 });
