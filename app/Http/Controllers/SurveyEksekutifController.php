0dcdf7a985d5392099790dd924881258bc75e9d7
Syam Apriansyah <syamapriansyah@gmail.com>
2023-02-22 14:12:53 +0700

    penambahan logo dan action ke database

--- app/Http/Controllers/SurveyEksekutifController.php
+++ app/Http/Controllers/SurveyEksekutifController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Session\Session;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
 
 class SurveyEksekutifController extends Controller
 {
     public function form()
     {
+        // dd('sini');
         return view('tulip/index');
+    }
+
+    public function action(Request $request){
+        $data = $request->all();
+        unset($data['_token']);
+        $status = DB::table('survey_pelanggan')->insert($data);
+        // dd($request);
+        return Redirect::back()->with(['success' => 'Data Berhasil Di Perbarui!']);
     }
 }
