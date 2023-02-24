8607e9dd901346f013035c541080119dbed944f6
Syam Apriansyah <syamapriansyah@gmail.com>
2023-02-24 11:22:12 +0700

    commit lagi biar ketarik di hosting

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SurveyEksekutifController;
 
-
 Route::controller(SurveyEksekutifController::class)->group(function () {
     Route::get('/', 'form')->name('tulip.form');
     Route::post('/action', 'action')->name('tulip.action');
 });
-// Route::view('/', 'tulip/index');
